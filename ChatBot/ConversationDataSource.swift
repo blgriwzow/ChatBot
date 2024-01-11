0c3903169be017a1a1911dd31f6d984d7f2acc24
Blgriwzow <blgriwzow@pima.edu>
2024-01-11 15:58:47 -0700

    Updated ChatDelegate and aesthetic attributes.

--- ChatBot/ConversationDataSource.swift
+++ ChatBot/ConversationDataSource.swift
@@ -2,23 +2,31 @@ import Foundation
 class ConversationDataSource {
     
     /// The number of Messages in the conversation
-    var messageCount = 0
+    var messageCount: Int {
+        return messages.count
+    }
+    /// Initializes an array with a starting message.
+    var messages = [openingLine]
     
     /// Add a new question to the conversation
     func add(question: String) {
         print("Asked to add question: \(question)")
-        messageCount += 1
+        let message = Message(date: Date(), text: question, type: .question)
+        messages.append(message)
+
     }
     
     /// Add a new answer to the conversation
     func add(answer: String) {
         print("Asked to add answer: \(answer)")
-        messageCount += 1
+        let message = Message(date: Date(), text: answer, type: .answer)
+        messages.append(message)
+        
     }
     
     /// The Message at a specific point in the conversation
     func messageAt(index: Int) -> Message {
         print("Asking for message at index \(index)")
-        return Message(date: Date(), text: "Hello, world!", type: .answer)
+        return messages[index]
     }
 }
--- ChatBot/ConversationDelegate.swift
+++ ChatBot/ConversationDelegate.swift
@@ -4,21 +4,28 @@ struct ConversationDelegate {
         let lowerQuestion = question.lowercased()
         
         if lowerQuestion.hasPrefix("hello") {
-            return "Why, hello there!"
-        } else if lowerQuestion == "where are the cookies?" {
-            return "In the cookie jar!"
+            return "Why... hello... there!"
+        } else if lowerQuestion.contains("sloth"){
+            return "...hey..! That's... me..."
+        }else if lowerQuestion == "where are the cookies?" {
+            return "...in the cookie jar!"
         } else if lowerQuestion.hasPrefix("where") {
-            return "To the North!"
+            return "...To the...\n ...North!"
+        } else if lowerQuestion.contains("your name"){
+            return "...my name? ...it's...\n Sloth...Bot"
+        }else if lowerQuestion.contains("cheat"){
+            return "... \n... \n...I'll never tell!"
         } else {
             let defaultNumber = question.count % 3
             
             if defaultNumber == 0 {
-                return "That really depends"
+                return "That really... depends..."
             } else if defaultNumber == 1 {
-                return "I think so, yes"
+                return "I think so... yes..."
             } else {
-                return "Ask me again tomorrow"
+                return "Ask me again... \n...tomorrow..."
             }
         }
     }
+    
 }
